ff2eab7c58c4a3356df2d1a23a16d7f6ebcc5b76
David M. Lloyd <david.lloyd@redhat.com>
2016-05-10 09:46:33 -0500

    Update transaction interceptor to detect and associate the transaction based on the configured policy

--- src/main/java/org/jboss/ejb/client/TransactionInterceptor.java
+++ src/main/java/org/jboss/ejb/client/TransactionInterceptor.java
@@ -22,16 +22,72 @@
 
 package org.jboss.ejb.client;
 
+import static java.security.AccessController.doPrivileged;
+
+import java.security.PrivilegedAction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.Transaction;
+import javax.transaction.TransactionManager;
+import javax.transaction.xa.Xid;
+
+import org.jboss.ejb._private.Logs;
+import org.jboss.ejb.client.annotation.ClientTransactionPolicy;
+import org.wildfly.common.Assert;
+import org.wildfly.transaction.client.RemoteTransactionContext;
+
 /**
- * The client interceptor which associates the current transaction ID with the invocation.
+ * The client interceptor which associates the current transaction with the invocation.
  *
  * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
  */
 public final class TransactionInterceptor implements EJBClientInterceptor {
+    private static final RemoteTransactionContext TRANSACTION_SYSTEM = doPrivileged((PrivilegedAction<RemoteTransactionContext>) RemoteTransactionContext::getInstance);
+
+    private final Supplier<TransactionManager> transactionManagerSupplier;
+    private final Function<Transaction, Xid> xidMapper;
+
+    public TransactionInterceptor(final Supplier<TransactionManager> transactionManagerSupplier, final Function<Transaction, Xid> xidMapper) {
+        Assert.checkNotNullParam("transactionManagerSupplier", transactionManagerSupplier);
+        Assert.checkNotNullParam("xidMapper", xidMapper);
+        this.transactionManagerSupplier = transactionManagerSupplier;
+        this.xidMapper = xidMapper;
+    }
 
     public void handleInvocation(final EJBClientInvocationContext context) throws Exception {
-        // TODO: use transaction client API to acquire transaction ID to attach
+        ClientTransactionPolicy transactionPolicy = context.getTransactionPolicy();
+        if (transactionPolicy == null) {
+            // TODO: per-thread and/or global default settings
+            transactionPolicy = ClientTransactionPolicy.SUPPORTS;
+        }
+        final TransactionManager transactionManager = transactionManagerSupplier.get();
+        final Transaction transaction = safeGetTransaction(transactionManager);
+
+        if (transactionPolicy.failIfTransactionAbsent()) {
+            if (transaction == null) {
+                throw Logs.TXN.txNotActiveForThread();
+            }
+        }
+        if (transactionPolicy.failIfTransactionPresent()) {
+            if (transaction != null) {
+                throw Logs.TXN.txAlreadyAssociatedWithThread();
+            }
+        }
+        if (transactionPolicy.propagate()) {
+            context.setTransaction(transaction);
+        }
         context.sendRequest();
+    }
+
+    private static Transaction safeGetTransaction(final TransactionManager transactionManager) {
+        try {
+            return transactionManager == null ? null : transactionManager.getTransaction();
+        } catch (SystemException e) {
+            return null;
+        }
     }
 
     public Object handleInvocationResult(final EJBClientInvocationContext context) throws Exception {
