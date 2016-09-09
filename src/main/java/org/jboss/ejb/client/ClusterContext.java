15eabdb7473c905c1a25acef93393030fe88cbb0
Radovan Stancel <rstancel@redhat.com>
2016-09-09 17:44:48 +0200

    [EJBCLIENT-146] - While reconnecting, close old receiver first if exists - checkstyle fixes

--- src/main/java/org/jboss/ejb/client/ClusterContext.java
+++ src/main/java/org/jboss/ejb/client/ClusterContext.java
@@ -24,8 +24,18 @@ package org.jboss.ejb.client;
 
 import org.jboss.logging.Logger;
 
-import java.util.*;
-import java.util.concurrent.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A {@link ClusterContext} keeps track of a specific cluster and the {@link org.jboss.ejb.client.remoting.ClusterNode}s
