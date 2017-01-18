313cfd8013117f03d08bf55a0e102e40f7df7815
David M. Lloyd <david.lloyd@redhat.com>
2017-01-18 10:29:39 -0600

    Add missing JavaDoc and validation to EJBIdentifier

--- src/main/java/org/jboss/ejb/client/EJBIdentifier.java
+++ src/main/java/org/jboss/ejb/client/EJBIdentifier.java
@@ -28,6 +28,11 @@ import java.util.Objects;
 import org.wildfly.common.Assert;
 
 /**
+ * An identifier for an EJB located within a container.  This identifier only names the EJB; it does not specify
+ * a view, which must be done using the {@link EJBLocator} family of types.
+ * <p>
+ * EJB identifiers are suitable for use as hash keys.
+ *
  * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
  */
 public final class EJBIdentifier implements Serializable {
@@ -39,10 +44,20 @@ public final class EJBIdentifier implements Serializable {
     private final String distinctName;
     private transient int hashCode;
 
+    /**
+     * Construct a new instance.
+     *
+     * @param appName the application name (must not be {@code null})
+     * @param moduleName the module name (must not be {@code null} or empty)
+     * @param beanName the bean name (must not be {@code null} or empty)
+     * @param distinctName the distinct name (must not be {@code null})
+     */
     public EJBIdentifier(final String appName, final String moduleName, final String beanName, final String distinctName) {
         Assert.checkNotNullParam("appName", appName);
         Assert.checkNotNullParam("moduleName", moduleName);
+        Assert.checkNotEmptyParam("moduleName", moduleName);
         Assert.checkNotNullParam("beanName", beanName);
+        Assert.checkNotEmptyParam("beanName", beanName);
         Assert.checkNotNullParam("distinctName", distinctName);
         this.appName = appName;
         this.moduleName = moduleName;
@@ -50,26 +65,58 @@ public final class EJBIdentifier implements Serializable {
         this.distinctName = distinctName;
     }
 
+    /**
+     * Get the application name, which may be empty.
+     *
+     * @return the application name (not {@code null})
+     */
     public String getAppName() {
         return appName;
     }
 
+    /**
+     * Get the module name.
+     *
+     * @return the module name (not {@code null})
+     */
     public String getModuleName() {
         return moduleName;
     }
 
+    /**
+     * Get the bean name.
+     *
+     * @return the bean name (not {@code null})
+     */
     public String getBeanName() {
         return beanName;
     }
 
+    /**
+     * Get the distinct name.
+     *
+     * @return the distinct name (not {@code null})
+     */
     public String getDistinctName() {
         return distinctName;
     }
 
+    /**
+     * Determine if this EJB identifier is equal to the given object.
+     *
+     * @param other the object to test
+     * @return {@code true} if the object is equal to this one, {@code false} otherwise
+     */
     public boolean equals(final Object other) {
         return other instanceof EJBIdentifier && equals((EJBIdentifier) other);
     }
 
+    /**
+     * Determine if this EJB identifier is equal to the given object.
+     *
+     * @param other the object to test
+     * @return {@code true} if the object is equal to this one, {@code false} otherwise
+     */
     public boolean equals(final EJBIdentifier other) {
         return other != null && (
             other == this ||
@@ -81,6 +128,11 @@ public final class EJBIdentifier implements Serializable {
         );
     }
 
+    /**
+     * Get the hash code of this identifier.
+     *
+     * @return the hash code of this identifier
+     */
     public int hashCode() {
         int hashCode = this.hashCode;
         if (hashCode != 0) {
@@ -90,6 +142,11 @@ public final class EJBIdentifier implements Serializable {
         return this.hashCode = hashCode == 0 ? 1 : hashCode;
     }
 
+    /**
+     * Get the EJB identifier as a human-readable string.
+     *
+     * @return the EJB identifier as a human-readable string (not {@code null})
+     */
     public String toString() {
         final String distinctName = getDistinctName();
         if (distinctName == null || distinctName.isEmpty()) {
