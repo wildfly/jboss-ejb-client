81a92335cb5fbf10417b3dea8bfacee84005a6a2
Tomasz Adamski <tadamski@redhat.com>
2019-06-17 23:47:59 +0200

    TransactionTestCase quickfix

--- src/test/java/org/jboss/ejb/client/test/TransactionTestCase.java
+++ src/test/java/org/jboss/ejb/client/test/TransactionTestCase.java
@@ -37,6 +37,8 @@ import org.jboss.ejb.client.test.common.EchoBean;
 import org.jboss.ejb.client.test.common.StatefulEchoBean;
 import org.jboss.ejb.client.test.common.StatelessEchoBean;
 import org.jboss.logging.Logger;
+import org.jboss.tm.XAResourceRecovery;
+import org.jboss.tm.XAResourceRecoveryRegistry;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -81,6 +83,16 @@ public class TransactionTestCase extends AbstractEJBClientTestCase {
         builder.setXATerminator(xat).setExtendedJBossXATerminator(xat);
         builder.setTransactionManager(narayanaTm);
         builder.setTransactionSynchronizationRegistry(narayanaTsr);
+        builder.setXAResourceRecoveryRegistry(new XAResourceRecoveryRegistry() {
+            @Override
+            public void addXAResourceRecovery(XAResourceRecovery xaResourceRecovery) {
+            }
+
+            @Override
+            public void removeXAResourceRecovery(XAResourceRecovery xaResourceRecovery) {
+            }
+        });
+        builder.build();
         LocalTransactionContext.getContextManager().setGlobalDefault(new LocalTransactionContext(builder.build()));
         txManager = ContextTransactionManager.getInstance();
         txSyncRegistry = ContextTransactionSynchronizationRegistry.getInstance();
