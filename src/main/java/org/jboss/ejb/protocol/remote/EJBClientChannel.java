3f31cbf1961d86969cb7447787f4b55419dd440b
Tomasz Adamski <tadamski@redhat.com>
2019-07-19 18:44:05 +0200

    [EJBCLIENT-340] Downgrade info log

--- src/main/java/org/jboss/ejb/protocol/remote/EJBClientChannel.java
+++ src/main/java/org/jboss/ejb/protocol/remote/EJBClientChannel.java
@@ -573,7 +573,9 @@ class EJBClientChannel {
     private XAOutflowHandle writeTransaction(final Transaction transaction, final DataOutput dataOutput,
             final AuthenticationContext authenticationContext) throws IOException, RollbackException, SystemException {
         if (authenticationContext != null) {
-            Logs.MAIN.info("Using existing AuthenticationContext for writeTransaction(...)");
+            if (Logs.MAIN.isDebugEnabled()) {
+                Logs.MAIN.debug("Using existing AuthenticationContext for writeTransaction(...)");
+            }
             try {
                 return authenticationContext.run((PrivilegedExceptionAction<XAOutflowHandle>) () -> _writeTransaction(transaction, dataOutput));
             } catch (PrivilegedActionException e) {
@@ -588,7 +590,9 @@ class EJBClientChannel {
                 throw new RuntimeException(e);
             }
         } else {
-            Logs.MAIN.info("No existing AuthenticationContext for writeTransaction(...)");
+            if (Logs.MAIN.isDebugEnabled()) {
+                Logs.MAIN.debug("No existing AuthenticationContext for writeTransaction(...)");
+            }
             return _writeTransaction(transaction, dataOutput);
         }
     }
