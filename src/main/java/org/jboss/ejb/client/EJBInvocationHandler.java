32340bc1d12bffa0e47fe8d2142d88a88929a5cc
Richard Opálka <opalka.richard@gmail.com>
2021-02-10 11:22:45 +0100

    [EJBCLIENT-400] Refactoring classes & decreasing visibility

--- src/main/java/org/jboss/ejb/client/EJBInvocationHandler.java
+++ src/main/java/org/jboss/ejb/client/EJBInvocationHandler.java
@@ -33,7 +33,6 @@ import javax.ejb.EJBHome;
 import javax.ejb.EJBObject;
 
 import org.jboss.ejb._private.Logs;
-import org.jboss.ejb._private.SystemProperties;
 import org.wildfly.common.Assert;
 import org.wildfly.discovery.Discovery;
 import org.wildfly.security.auth.client.AuthenticationContext;
@@ -50,7 +49,7 @@ final class EJBInvocationHandler<T> extends Attachable implements InvocationHand
 
     private static final Supplier<Discovery> DISCOVERY_SUPPLIER = doPrivileged((PrivilegedAction<Supplier<Discovery>>) Discovery.getContextManager()::getPrivilegedSupplier);
 
-    private static final int MAX_RETRIES = SystemProperties.getInteger(SystemProperties.MAX_ENTRIES, 8);
+    private static final int MAX_RETRIES = SecurityUtils.getInteger(SystemProperties.MAX_ENTRIES, 8);
 
     private final transient boolean async;
 
