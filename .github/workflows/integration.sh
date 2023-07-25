3f02471f3f221628616f7ffd06fc5c44bc12884d
Tomasz Adamski <tadamski@redhat.com>
2023-07-25 16:24:22 +0200

    [EJBCLIENT-505] CI Add integration testing to CI script (#644)

---
+++ .github/workflows/integration.sh
@@ -0,0 +1,12 @@
+echo "INTEGRATION TESTS"
+
+EJB_CLIENT_REPOSITORY=$1
+EJB_CLIENT_BRANCH=$2
+
+git clone --depth=1 https://github.com/wildfly/ejb-client-testsuite
+
+cd ejb-client-testsuite
+
+mvn -B -ntp package -DspecificModule=prepare -Dejb.client.repository=${EJB_CLIENT_REPOSITORY} -Dejb.client.branch=${EJB_CLIENT_BRANCH}
+mvn -B -ntp dependency:tree clean verify --fail-at-end
+
